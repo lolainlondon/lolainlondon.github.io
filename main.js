5c1afa4c14e79248e348f829190443a618acfe2a
lolainlondon <134396043+lolainlondon@users.noreply.github.com>
2023-06-17 11:00:58 +0100

    added command to place script

---
+++ main.js
@@ -0,0 +1,22 @@
+//Define arrays that contain quote segments
+let beginningQuote = ['Never forget to', 'You should always', 'Take time to', 'It can help to']
+let middleQuote = ['be at one with nature', 'appreciate little joys', 'listen to your inner child', 'breathe deeply']
+let endQuote = ['when searching for your path', 'even during large sorrows', 'at the start of a steep climb', 'in crowded places']
+
+//Define randomizer
+const selectQuote = arr => {
+  const quoteIndex = Math.floor(Math.random()*4)
+  {return arr[quoteIndex]}
+};
+const allQuotes = () => {
+console.log(selectQuote(beginningQuote))
+console.log(selectQuote(middleQuote))
+console.log(selectQuote(endQuote))
+}
+let beginning = selectQuote(beginningQuote);
+let middle = selectQuote(middleQuote);
+let end = selectQuote(endQuote);
+
+document.getElementById('beginning').innerHTML = beginning;
+document.getElementById('middle').innerHTML = middle;
+document.getElementById('end').innerHTML = end;
